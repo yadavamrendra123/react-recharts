4d19276099865bb077e5d13795e0623b39382382
yadavamrendra123 <tha075bei005@tcioe.edu.np>
2022-12-19 11:23:18 +0545

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Chart from './Components/Chart'
+export default function App() {
+  const [data, setdata] = useState();
 
+  useEffect(() => {
+    const fetchDatas = async () => {
+      const res = await fetch("https://api.coincap.io/v2/assets/?limit=20");
+      const data = await res.json();
+      console.log(data);
+      setdata(data?.data);
+    };
+    fetchDatas();
+  }, []);
 
-function App() {
-
-
-  return (
-    <div className="App">
-        React-Recharts
-    </div>
+  return(
+      <div className="App">
+        <Chart data={data}/>
+      </div>
   )
-}
 
-export default App
+}
